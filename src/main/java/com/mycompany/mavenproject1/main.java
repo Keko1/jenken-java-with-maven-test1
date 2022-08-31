b79caaef5e08f5fce1a55f2a0b5afbff124d3d67
Keko1 <keko.joo@gmail.com>
2022-08-31 13:13:21 +0200

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1/main.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject1;
+/**
+ * @author Kirollos.Nabil
+ */
+public class main {
+   public static void main(String[] args) {
+        System.out.println("Hello Connect PS");
+ }
+}
